a2e57afe6a54aa44098a40f2b201192fbcff91da
Vlad Muresan <padawan4330@gmail.com>
2022-12-21 22:50:45 +0200

    Remove the implementation of CardRequestService.get_expired_card_requests

--- M3RC_TOOLS.py
+++ M3RC_TOOLS.py
@@ -14,10 +14,11 @@ PASSWORD = os.getenv('GOOGLE_PLAY_PASSWORD')
 
 SPPD_API.setUsernamePassword(USERNAME, PASSWORD)
 
-test_time_delta = datetime.timedelta(hours=2)
+test_time_delta = datetime.timedelta(hours=1)
 card_request_service = CardRequestService()
+
+print(human_friendlyfy_card_requests(card_request_service.card_requests))
 
 unfilled_card_requests_about_to_expire = card_request_service.get_unfilled_card_requests_about_to_expire(test_time_delta)
 print(human_friendlyfy_card_requests(unfilled_card_requests_about_to_expire))
-expired_card_requests = card_request_service.get_expired_card_requests(test_time_delta)
-print(human_friendlyfy_card_requests(expired_card_requests))
+
--- service/CARD_REQUEST_SERVICE.py
+++ service/CARD_REQUEST_SERVICE.py
@@ -5,11 +5,12 @@ import SPPD_API
 
 
 class CardRequestService:
+    card_requests = None
     unfilled_card_requests = None
 
-    # @staticmethod
     def __init__(self):
-        self.unfilled_card_requests = CardRequestService.__get_unfilled_card_requests()
+        self.card_requests = CardRequestService.__get_card_requests()
+        self.unfilled_card_requests = self.__get_unfilled_card_requests()
 
     @staticmethod
     def __get_card_requests():
@@ -18,10 +19,9 @@ class CardRequestService:
         card_requests = parsed_response['requests']
         return card_requests
 
-    @staticmethod
-    def __get_unfilled_card_requests():
+    def __get_unfilled_card_requests(self):
         filtered = filter(lambda request: request['max_copies'] > request['copies'],
-                          CardRequestService.__get_card_requests())
+                          self.card_requests)
         return list(filtered)
 
     def get_unfilled_card_requests_about_to_expire(self, expiry_time_delta):
@@ -35,16 +35,7 @@ class CardRequestService:
 
         return unfilled_card_requests_about_to_expire
 
+    #Expired card requests are not sent via the SPPD Api
     def get_expired_card_requests(self, expiry_time_delta):
+        raise NotImplementedError("Not implemented yet")
 
-        expiry_time_interval_start = datetime.datetime.now() - expiry_time_delta
-        expiry_time_interval_end = datetime.datetime.now()
-        expired_card_requests = []
-
-        for cr in self.unfilled_card_requests:
-            cr_valid_until_datetime = datetime.datetime.fromtimestamp(cr['valid_until'])
-            if (expiry_time_interval_start < cr_valid_until_datetime <= expiry_time_interval_end) \
-                    and (cr['max_copies'] != cr['copies']):
-                expired_card_requests.append(cr)
-
-        return expired_card_requests
