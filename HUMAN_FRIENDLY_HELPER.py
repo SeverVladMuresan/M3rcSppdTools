c7e624f5635a135dfda7694179de11c9474e356c
Vlad Muresan <padawan4330@gmail.com>
2022-12-21 23:57:31 +0200

    Add 'expiry' parameter and rename iteration variable in human_friendlyfy_card_requests

--- HUMAN_FRIENDLY_HELPER.py
+++ HUMAN_FRIENDLY_HELPER.py
@@ -5,16 +5,20 @@ from service.CARD_NAME_SERVICE import CardNames
 
 
 def human_friendlyfy_card_requests(card_requests):
-    for r in card_requests:
-        r['valid_until_human_friendly'] = get_human_friendly_date(r['valid_until'])
-        r['created_human_friendly'] = get_human_friendly_date(r['created'])
-        r['card_name'] = get_human_friendly_card_name(r['card_id'])
-        r['profile_name'] = get_human_friendly_username(r['profile_id'])
+    now_datetime = datetime.datetime.now()
+    for cr in card_requests:
+        cr['valid_until_human_friendly'] = get_human_friendly_date(cr['valid_until'])
+        cr['created_human_friendly'] = get_human_friendly_date(cr['created'])
+        cr['card_name'] = get_human_friendly_card_name(cr['card_id'])
+        cr['profile_name'] = get_human_friendly_username(cr['profile_id'])
+        cr_valid_until_datetime = datetime.datetime.fromtimestamp(cr['valid_until'])
+        expiry = cr_valid_until_datetime - now_datetime
+        cr['expiry'] = expiry.__str__()
     return card_requests
 
 
 def get_human_friendly_username(profile_id):
-    parsed_response = SPPD_API.getUserName(profile_id);
+    parsed_response = SPPD_API.getUserName(profile_id)
     return parsed_response['profiles'][0]['nameOnPlatform']
 
 
