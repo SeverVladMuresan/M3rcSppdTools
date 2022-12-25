74c05a2802b2c40fcada9c3a100bc843786931ae
Vlad Muresan <padawan4330@gmail.com>
2022-12-25 17:36:58 +0200

    Modify M3RC_TOOLS.py to handle the discord bot interaction

--- M3RC_TOOLS.py
+++ M3RC_TOOLS.py
@@ -1,24 +1,52 @@
+import asyncio
 import datetime
 import os
 
 import SPPD_API
-from HUMAN_FRIENDLY_HELPER import human_friendlyfy_card_requests
+from service.BOT_MESSAGE_SERVICE import BotMessageService
 from service.CARD_REQUEST_SERVICE import CardRequestService
+import discord
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
+
+from service.SCHEDULING_SERVICE import SchedulingService
+
+TIME_DELTAS = []
+for i in [2, 1]:
+    expiry_time_delta = datetime.timedelta(hours=i)
+    TIME_DELTAS.append(expiry_time_delta)
+
+
+@tasks.loop(minutes=30)
+async def bot_action(ctx):
+    card_request_service = CardRequestService()
+    unfilled_card_requests_about_to_expire = card_request_service.get_unfilled_card_requests_about_to_expire(TIME_DELTAS)
+    await ctx.send(BotMessageService.build_message(unfilled_card_requests_about_to_expire))
+
+
+@bot_action.before_loop
+async def before_bot_action():
+    future = SchedulingService.get_next_even_half_hour()
+
+    await asyncio.sleep((future-datetime.datetime.now()).seconds)
 
 load_dotenv()
 
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 USERNAME = os.getenv('GOOGLE_PLAY_USERNAME')
 PASSWORD = os.getenv('GOOGLE_PLAY_PASSWORD')
 
-
 SPPD_API.setUsernamePassword(USERNAME, PASSWORD)
 
-test_time_delta = datetime.timedelta(hours=1)
-card_request_service = CardRequestService()
+intents = discord.Intents.default()
+intents.message_content = True
 
-print(human_friendlyfy_card_requests(card_request_service.card_requests))
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-unfilled_card_requests_about_to_expire = card_request_service.get_unfilled_card_requests_about_to_expire(test_time_delta)
-print(human_friendlyfy_card_requests(unfilled_card_requests_about_to_expire))
+
+@bot.command(name='start')
+async def start_bot(ctx):
+    bot_action.start(ctx)
+
+bot.run(DISCORD_TOKEN)
 
